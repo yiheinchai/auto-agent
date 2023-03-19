20fa9f124435e02a73eb98266e61e3d1bd36233d
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2023-03-19 22:55:15 +0000

    add error handling and re matching

--- autonomous.py
+++ autonomous.py
@@ -5,28 +5,38 @@ If the output from the LLM contains instruction to add or modify a method to cla
 If the output from the LLM contains instruction to execute the code contained within the output, then execute the code. 
 Add another method which enables the class to interface with the ChatGPT API. Whenever this method is called, ensure to pass the output into the execute code method.
 """
+import re
 
 class AutonomousLLM:
     def __init__(self):
         self.chatgpt = ChatGPT()
 
     def execute_code(self, output):
-        if "add method" in output:
-            method_name = output.split(" ")[-1]
-            method_code = output.split("add method ")[-1].split(" to class")[0]
-            setattr(self, method_name, method_code)
-            print(f"Method {method_name} added to object.")
-        elif "modify method" in output:
-            method_name = output.split(" ")[-1]
-            method_code = output.split("modify method ")[-1].split(" to class")[0]
-            setattr(self, method_name, method_code)
-            print(f"Method {method_name} modified in object.")
-        elif "execute code" in output:
-            code = output.split("execute code ")[-1]
-            exec(code)
-            print("Code executed.")
-        else:
-            print("Invalid output.")
+        add_method_pattern = r"add method (\w+) to class:\n([\s\S]+)"
+        modify_method_pattern = r"modify method (\w+) in class:\n([\s\S]+)"
+        execute_code_pattern = r"execute code:\n([\s\S]+)"
+
+        try:
+            add_method_match = re.match(add_method_pattern, output)
+            modify_method_match = re.match(modify_method_pattern, output)
+            execute_code_match = re.match(execute_code_pattern, output)
+
+            if add_method_match:
+                method_name, method_code = add_method_match.groups()
+                self.methods[method_name] = method_code
+                print(f"Method {method_name} added to object.")
+            elif modify_method_match:
+                method_name, method_code = modify_method_match.groups()
+                self.methods[method_name] = method_code
+                print(f"Method {method_name} modified in object.")
+            elif execute_code_match:
+                code = execute_code_match.group(1)
+                exec(code)
+                print("Code executed.")
+            else:
+                print("Invalid output.")
+        except Exception as e:
+            print(f"Error: {e}")
 
     def interface_chatgpt(self, input):
         response = self.chatgpt.get_response(input)
