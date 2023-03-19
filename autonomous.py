4c3b4a3b9850bb84f8c74bb53787a38601d2d5d0
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2023-03-19 22:16:38 +0000

    initial commit

---
+++ autonomous.py
@@ -0,0 +1,32 @@
+"""
+PROMPT:
+Write a python class which has a method called execute code which takes an output from an LLM. 
+If the output from the LLM contains instruction to add or modify a method to class, take the code from the LLM and add/modify the method to the class. 
+If the output from the LLM contains instruction to execute the code contained within the output, then execute the code. 
+Add another method which enables the class to interface with the ChatGPT API.
+"""
+
+class AutonomousLLM:
+    def __init__(self):
+        self.chatgpt = ChatGPT()
+
+    def execute_code(self, output):
+        if "add method" in output:
+            method_name = output.split(" ")[-1]
+            method_code = output.split("add method ")[-1].split(" to class")[0]
+            setattr(self, method_name, method_code)
+            print(f"Method {method_name} added to object.")
+        elif "modify method" in output:
+            method_name = output.split(" ")[-1]
+            method_code = output.split("modify method ")[-1].split(" to class")[0]
+            setattr(self, method_name, method_code)
+            print(f"Method {method_name} modified in object.")
+        elif "execute code" in output:
+            code = output.split("execute code ")[-1]
+            exec(code)
+            print("Code executed.")
+        else:
+            print("Invalid output.")
+
+    def interface_chatgpt(self, input):
+        return self.chatgpt.get_response(input)
