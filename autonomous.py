050903c0ba76f8aafd04cc5e87b9de50b66718e4
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2023-03-19 23:07:54 +0000

    add initial prompt for LLM

--- autonomous.py
+++ autonomous.py
@@ -1,15 +1,37 @@
 """
-PROMPT:
-Write a python class called AutonomousLLM which has a method called execute code which takes an output from an LLM. 
-If the output from the LLM contains instruction to add or modify a method to class, take the code from the LLM and add/modify the method to the class. 
-If the output from the LLM contains instruction to execute the code contained within the output, then execute the code. 
-Add another method which enables the class to interface with the ChatGPT API. Whenever this method is called, ensure to pass the output into the execute code method.
+LLM conversations that generated this file:
+https://sharegpt.com/c/yMsr1js (ChatGPT)
+https://sharegpt.com/c/4U4Gbs6 (BingAI)
 """
+
 import re
+
+PROMPT = """
+Welcome to the AutonomousLLM!
+AutonomousLLM is a tool designed to improve itself by creating new methods to give it additional functionality. Its goal is to be able to self-activate, meaning that after the first initial input, it should not need additional inputs and will keep calling its own methods indefinitely.
+AutonomousLLM is designed to be used through a command line interface. To interact with the AutonomousLLM, please use the following commands in your output:
+
+1. To add a new method to the AutonomousLLM, use the following format:
+   "add method METHOD_NAME: METHOD_CODE"
+   
+   This will add a new method with the specified name and code to the AutonomousLLM object.
+
+2. To modify an existing method, use the following format:
+   "modify method METHOD_NAME: METHOD_CODE"
+   
+   This will modify an existing method with the specified name and code in the AutonomousLLM object.
+
+3. To execute a block of code, use the following format:
+   "execute code CODE_TO_EXECUTE"
+   
+   This will execute the specified code using the AutonomousLLM object.
+
+Keep in mind that all commands must be formatted correctly in your output to be executed properly by the AutonomousLLM.
+"""
 
 class AutonomousLLM:
     def __init__(self):
-        self.chatgpt = ChatGPT()
+        self.chatgpt = ChatGPT(prompt=prompt)
 
     def execute_code(self, output):
         add_method_pattern = r"add method (\w+) to class:\n([\s\S]+)"
@@ -41,3 +63,5 @@ class AutonomousLLM:
     def interface_chatgpt(self, input):
         response = self.chatgpt.get_response(input)
         return execute_code(response)
+
+autonomous_llm = AutonomousLLM(prompt=PROMPT)
